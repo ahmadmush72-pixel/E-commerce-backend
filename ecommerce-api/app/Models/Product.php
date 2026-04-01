04eaba4b93902afd7a3f171e2d6e1d0e3985462e
Mushtaq Ahmad <ahmadmush72@gmail.com>
2026-04-01 15:34:04 +0500

    model fillable

--- ecommerce-api/app/Models/Product.php
+++ ecommerce-api/app/Models/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'stock',
+        'image',
+        'status',
+    ];
+
+   
 }
